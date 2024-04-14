11844df397261ff5b158d35858641f50c1fa067a
Vishalku88 <164476507+Vishalku88@users.noreply.github.com>
2024-04-14 10:05:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,8 +75,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "vishalku8765@cluster0.nnxqvfk.mongodb.net")
-DATABASE_NAME = environ.get('DATABASE_NAME', "bisal-ki-bot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vishalku258258:vishalku2588@cluster0.bc1okyy.mongodb.net/")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Demobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
